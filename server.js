b97999c22364abca1d5fab253ca1c1df8f2e1b89
Sam <darosamy@gmail.com>
2024-05-11 15:05:46 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,6 +1,2 @@
-const strapi = require('@strapi');
-
-if (process.env.NODE_ENV == "development")
-    strapi({"autoReload": { "enabled": true }}).start();
-else
-    strapi().start();
+const strapi = require('@strapi/strapi');
+strapi(/* {...} */).start();
